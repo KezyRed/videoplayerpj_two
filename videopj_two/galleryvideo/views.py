250cf2398c8e65f9ea33156f70b45d6524d1b776
kezy <kezy@example.com>
2025-04-16 00:13:14 +0300

    commit: add admin site galleryvideo

--- videopj_two/galleryvideo/views.py
+++ videopj_two/galleryvideo/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Gallery, Video
 
 # Create your views here.
+def index(request):
+    videos = Video.objects.all()
+    return render(request, 'galleryvideo/index.html', {'videos': videos})
+
+def video_player(request, video_id):
+    video = get_object_or_404(Video, pk=video_id)
+    return render(request, 'galleryvideo/video_player.html', {'video': video})
