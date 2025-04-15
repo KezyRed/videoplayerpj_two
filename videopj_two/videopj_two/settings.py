7e39e2f96e95dbd3545d11f4f43a2fe7f85ffe0c
kezy <kezy@example.com>
2025-04-15 13:54:14 +0300

    commit : update old version

--- videopj_two/videopj_two/settings.py
+++ videopj_two/videopj_two/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'videopj_two.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
