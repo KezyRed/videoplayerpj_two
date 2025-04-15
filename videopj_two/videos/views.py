4a007a059aaf5bff2534cf8e27ac295271240c2a
kezy <kezy@example.com>
2025-04-15 13:47:10 +0300

    commit: update view.py

--- videopj_two/videos/views.py
+++ videopj_two/videos/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Video
 
 # Create your views here.
+class VideoListView(ListView):
+    model = Video
+    template_name = 'videos/video_list.html'
+    context_object_name = 'videos'
+    ordering = ['-created_at']
+
+class VideoDetailView(DetailView):
+    model = Video
+    template_name = 'videos/video_detail.html'
+    context_object_name = 'video'
